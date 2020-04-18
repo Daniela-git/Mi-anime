09a3c913adaa33701026b4d17bcc1be995cc18dc
DanielaHA <dhiguitaa17@gmail.com>
2020-04-18 17:56:31 -0500

    creada clase api

--- js/api.js
+++ js/api.js
@@ -1,19 +1,30 @@
-class ApiAnime {
-	constructor(link) {
-		this.link = link;
-	}
+class Api{
+    async conseguirId(anime) {
 
-	async conseguirId(link) {
-		const respuesta = await fetch(link);
-
-		//pasando la respuesta a json
-		const datosAnime = await respuesta.json();
-		return datosAnime;
-    }
+        const link = `https://api.jikan.moe/v3/search/anime?q=${anime}&limit=1`;
+        //esperando la respuesta
+        let respuesta = await fetch(link);
     
-    async infoAnime(link) {
-        const respuesta = await fetch(link);
-        const datosAnime = await respuesta.json();
-		return datosAnime;
+        //pasando la respuesta a json
+        let datosAnime = await respuesta.json();
+        return datosAnime;
+        //usando los datos de la api
+    }
+    async infoAnime(animeId) {
+        const link = `https://api.jikan.moe/v3/anime/${animeId}`;
+        let respuesta = await fetch(link);
+    
+        //pasando la respuesta a json
+        let datosAnime = await respuesta.json();
+        return datosAnime;
+    }
+    async episodios(animeId){
+        const link = `https://api.jikan.moe/v3/anime/${animeId}/episodes`;
+        let respuesta = await fetch(link);
+        //pasando la respuesta a json
+        let datosAnime = await respuesta.json();
+        return datosAnime;
+    }
+
 
 }
--- js/app.js
+++ js/app.js
@@ -2,6 +2,8 @@
 //se podría sacar los id de los animes y guardarlos en un arreglo y así se ahorra el usa de una función y se consigue mejorar el tiempo de carga, y esa función pasaría a ser usada solamente cuando se agregue un nuevo anime, y se podrían hasta guardar en el local storage, pero de momento dejemolo en una lista
 
 //variables
+const api = new Api();
+
 const boton = document.getElementById("cargar");
 let misAnimes = [
 	"91 DAYS",
@@ -11,10 +13,10 @@ let misAnimes = [
 	"AMNESIA",
 	"ANGEL BEATS",
 	"ANOTHER",
-	"ANOHANA",
+	"naruto",
 ];
-
-let misAnimesId = [];
+let misAnimesId = []
+let misAnimesObjetos = [];
 
 //event listeners
 eventListeners();
@@ -25,37 +27,35 @@ function newFunction() {
 
 function eventListeners() {
 	document.addEventListener("DOMContentLoaded", buscadorDeAnime);
-	// boton.addEventListener("click", datosAnime);
+	boton.addEventListener("click", datosAnime);
 }
 
 //funciones
 
 function buscadorDeAnime(e) {
 	misAnimes.forEach((anime) => {
-		const link = `https://api.jikan.moe/v3/search/anime?q=${anime}&limit=1`;
-		conseguirId(link).then((anime) => {
+        //llamamos la funcion quenos devuelve el id del anime
+        api.conseguirId(anime)
+            .then((anime) => {
             const animeId = anime.results[0].mal_id;
-            const link = `https://api.jikan.moe/v3/anime/${animeId}`;
-			infoAnime(link).then(function (anime) {
-				console.log(anime);
-			});
+            misAnimesId.push(animeId);
+
+            //llamamos la funcion para obtener el resto de la informacion
+            api.infoAnime(animeId)
+                .then(function (anime) {
+                    misAnimesObjetos.push(anime);
+                    // console.log(anime)
+			    });
 		});
-	});
+    });
+    
 }
-async function conseguirId(link) {
-	//esperando la respuesta
-	let respuesta = await fetch(link);
 
-	//pasando la respuesta a json
-	let datosAnime = await respuesta.json();
-	return datosAnime;
-	//usando los datos de la api
-}
-async function infoAnime(link) {
-	let respuesta = await fetch(link);
+function datosAnime() {
+    api.episodios(misAnimesId[6])
+    .then(listaEpisodios=>{
+        console.log(listaEpisodios)
+    })
 
-	//pasando la respuesta a json
-	let datosAnime = await respuesta.json();
-	return datosAnime;
+
 }
-// console.log("----------------------");
