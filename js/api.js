c51b9048b5cc2b3aa52694d954c968d6328a3f31
DanielaHA <dhiguitaa17@gmail.com>
2020-04-19 17:54:28 -0500

    agregadas las listas de info de anime y funciona la api

--- js/api.js
+++ js/api.js
@@ -1,6 +1,6 @@
 class Api{
     async conseguirId(anime) {
-
+        
         const link = `https://api.jikan.moe/v3/search/anime?q=${anime}&limit=1`;
         //esperando la respuesta
         let respuesta = await fetch(link);
@@ -13,12 +13,12 @@ class Api{
     async infoAnime(animeId) {
         const link = `https://api.jikan.moe/v3/anime/${animeId}`;
         let respuesta = await fetch(link);
-    
+        
         //pasando la respuesta a json
         let datosAnime = await respuesta.json();
         return datosAnime;
     }
-    async episodios(animeId){
+    async listaEpisodios(animeId){
         const link = `https://api.jikan.moe/v3/anime/${animeId}/episodes`;
         let respuesta = await fetch(link);
         //pasando la respuesta a json
--- js/app.js
+++ js/app.js
@@ -2,28 +2,17 @@
 //se podría sacar los id de los animes y guardarlos en un arreglo y así se ahorra el usa de una función y se consigue mejorar el tiempo de carga, y esa función pasaría a ser usada solamente cuando se agregue un nuevo anime, y se podrían hasta guardar en el local storage, pero de momento dejemolo en una lista
 
 //variables
-const api = new Api();
 
-const boton = document.getElementById("cargar");
-let misAnimes = [
-	"91 DAYS",
-	"ACCHI KOCCHI",
-	"AKAGAMI NO SHIRAYUKI-HIME",
-	"AMAGI BRILLANT PARK",
-	"AMNESIA",
-	"ANGEL BEATS",
-	"ANOTHER",
-	"naruto",
-];
-let misAnimesId = []
+const api = new Api();
+let misAnimesId = [];
 let misAnimesObjetos = [];
 
+//selectores
+const boton = document.getElementById("cargar");
+const listaAnime = document.getElementById('lista-anime')
 //event listeners
 eventListeners();
 
-function newFunction() {
-	return [];
-}
 
 function eventListeners() {
 	document.addEventListener("DOMContentLoaded", buscadorDeAnime);
@@ -33,29 +22,38 @@ function eventListeners() {
 //funciones
 
 function buscadorDeAnime(e) {
-	misAnimes.forEach((anime) => {
-        //llamamos la funcion quenos devuelve el id del anime
-        api.conseguirId(anime)
-            .then((anime) => {
-            const animeId = anime.results[0].mal_id;
-            misAnimesId.push(animeId);
+	listaNombre.forEach((anime) => {
+		//llamamos la funcion quenos devuelve el id del anime
 
-            //llamamos la funcion para obtener el resto de la informacion
-            api.infoAnime(animeId)
-                .then(function (anime) {
-                    misAnimesObjetos.push(anime);
-                    // console.log(anime)
-			    });
-		});
-    });
-    
+		api.conseguirId(anime)
+			.then((anime) => {
+				const animeId = anime.results[0].mal_id;
+				misAnimesId.push(animeId);
+
+				//llamamos la funcion para obtener el resto de la informacion
+				setTimeout(()=>{
+					console.log('esperanding')
+					api.infoAnime(animeId)
+						.then(function (anime) {
+							misAnimesObjetos.push(anime);
+						})
+						.catch((error) => {
+							console.log("error desde infoAnime");
+						});
+
+				}),30000
+			})
+			.catch((error) => console.log("error conseguir id"));
+	});
+	console.log(misAnimesId);
 }
 
 function datosAnime() {
-    api.episodios(misAnimesId[6])
-    .then(listaEpisodios=>{
-        console.log(listaEpisodios)
-    })
+	let html = ''
+	misAnimesObjetos.forEach(anime=>{
+		html += `<li>${anime.title}</li>`
+	})
+	listaAnime.innerHTML= html
 
 
 }
