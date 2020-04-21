a6e137c2eafaec06f60846f0316c2c9cd8b31b92
DanielaHA <dhiguitaa17@gmail.com>
2020-04-20 21:08:06 -0500

    funcionan las imagenes

--- js/api.js
+++ js/api.js
@@ -1,32 +1,28 @@
-class Api{
-    async conseguirId(anime) {
-        
-        const link = `https://api.jikan.moe/v3/search/anime?q=${anime}&limit=1`;
-        //esperando la respuesta
-        let respuesta = await fetch(link);
-    
-        //pasando la respuesta a json
-        let datosAnime = await respuesta.json();
-        console.log('id')
-        return datosAnime;
-        //usando los datos de la api
-    }
-    async infoAnime(animeId) {
-        const link = `https://api.jikan.moe/v3/anime/${animeId}`;
-        let respuesta = await fetch(link);
-        
-        //pasando la respuesta a json
-        let datosAnime = await respuesta.json();
-        console.log('info')
-        return datosAnime;
-    }
-    async listaEpisodios(animeId){
-        const link = `https://api.jikan.moe/v3/anime/${animeId}/episodes`;
-        let respuesta = await fetch(link);
-        //pasando la respuesta a json
-        let datosAnime = await respuesta.json();
-        return datosAnime;
-    }
+class Api {
+	async conseguirId(anime) {
+		const link = `https://api.jikan.moe/v3/search/anime?q=${anime}&limit=1`;
 
+		//esperando la respuesta
+		let respuesta = await fetch(link);
 
+		//pasando la respuesta a json
+		let datosAnime = await respuesta.json();
+
+		return datosAnime;
+	}
+	async infoAnime(animeId) {
+		const link = `https://api.jikan.moe/v3/anime/${animeId}`;
+		let respuesta = await fetch(link);
+
+		//pasando la respuesta a json
+		let datosAnime = await respuesta.json();
+		return datosAnime;
+	}
+	async listaImagenes(animeId) {
+		const link = `https://api.jikan.moe/v3/anime/${animeId}/pictures`;
+		let respuesta = await fetch(link);
+		//pasando la respuesta a json
+		let datosAnime = await respuesta.json();
+		return datosAnime;
+	}
 }
--- js/app.js
+++ js/app.js
@@ -9,14 +9,15 @@ let misAnimesObjetos = [];
 let html = "";
 
 //selectores
-const boton = document.getElementById("cargar");
+const boton = document.querySelector(".boton");
 const listaAnime = document.getElementById("lista-anime");
 //event listeners
 eventListeners();
 
 function eventListeners() {
 	document.addEventListener("DOMContentLoaded", buscadorDeAnime);
-	// boton.addEventListener("click", datosAnime);
+	// document.addEventListener("DOMContentLoaded", prueba);
+	boton.addEventListener("click", datosAnime);
 }
 
 //funciones
@@ -24,22 +25,52 @@ function buscadorDeAnime(e) {
 	listaNombre.forEach(async function (anime) {
 		//llamamos la funcion quenos devuelve el id del anime
 		//funcion a crear
-		const resultBusqueda = await api.conseguirId(anime);
-		let animeId = resultBusqueda.results[0].mal_id;
-		// misAnimesId.push(animeId);
+
 		setTimeout(async () => {
+			const resultBusqueda = await api.conseguirId(anime);
+			let animeId = resultBusqueda.results[0].mal_id;
+
+			// misAnimesId.push(animeId);
 			const objetoAnime = await api.infoAnime(animeId);
-			// misAnimesObjetos.push(objetoAnime);
-			html += `<li>${objetoAnime.title}</li>`;
+			misAnimesObjetos.push(objetoAnime);
+			html += `<div class='elemento-anime' id='anime${misAnimesObjetos.length-1}'><li>${objetoAnime.title}</li></div>`;
 			listaAnime.innerHTML = html;
-		}, 2000);
+		}, 500);
 	});
 }
 
-// function datosAnime() {
-// 	let html = "";
-// 	misAnimesObjetos.forEach((anime) => {
-// 		html += `<li>${anime.title}</li>`;
-// 	});
-// 	listaAnime.innerHTML = html;
+async function datosAnime() {
+	// console.log(misAnimesObjetos.length)
+	misAnimesObjetos.forEach(async (anime, index) => {
+		setTimeout(async () => {
+			try{
+				let results = await api.listaImagenes(anime.mal_id);
+				let imagenes = results.pictures;
+				// console.log(imagenes);
+				let padre = document.querySelector(`#anime${index}`);
+				let img = document.createElement("img");
+				img.setAttribute("src", `${imagenes[0].small}`);
+				padre.appendChild(img);
+
+			}catch(err){
+				console.log(err)
+			}
+		}, 2000);
+		// console.log(li)
+	});
+}
+
+// async function prueba() {
+// 	console.time("time");
+// 	setTimeout(async () => {
+// 		const resultBusqueda = await api.conseguirId(listaAnime[12]);
+		
+// 		const animeId = resultBusqueda.results[0].mal_id
+		
+// 		const objetoAnime = await api.infoAnime(animeId);
+// 		// misAnimesObjetos.push(objetoAnime);
+// 		html += `<div class='elemento-anime ${objetoAnime.title}'><li>${objetoAnime.title}</li></div>`;
+// 		listaAnime.innerHTML = html;
+// 		console.timeEnd("time");
+// 	}, 2000);
 // }
